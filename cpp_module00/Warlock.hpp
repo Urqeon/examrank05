2130b4080c0ff4423f7be5aba81e542e7d7b2307
Alp Bulut <94965527+Urqeon@users.noreply.github.com>
2024-11-29 19:58:45 +0300

    Add files via upload

---
+++ cpp_module00/Warlock.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <iostream>
+
+class Warlock
+{
+private:
+	Warlock();
+	std::string _name;
+	std::string _title;
+
+public:
+	Warlock(const std::string &name, const std::string &title);
+	~Warlock();
+	std::string const &getName() const { return _name; }
+	std::string const &getTitle() const { return _title; }
+	void setTitle(std::string const &title) { _title = title; }
+	void introduce() const;
+};
